11fd84bd2699b9e5158a6a70e88fd10ffde55278
James Hughes <jh2284@cam.ac.uk>
2023-12-07 17:08:41 +0000

    Feat: Write solve_part_e.py, producing a basic plot

--- src/mixed_pdf_tools.py
+++ src/mixed_pdf_tools.py
@@ -7,9 +7,12 @@ import matplotlib.pyplot as plt
 
 def pdf_norm_expon_mixed(x, f, la, mu, sg, alpha, beta):
     pdf_s = norm.pdf(x, loc=mu, scale=sg)
-    pdf_b = expon.pdf(x, loc=0, scale=1/la)
-    weight_s = (2 * f) / (erf((beta - mu) / (sg*np.sqrt(2))) - erf((alpha - mu) / (sg*np.sqrt(2))))
-    weight_b = (1 - f) / (np.exp(- la * alpha) - np.exp(- la * beta))
+    pdf_b = expon.pdf(x, loc=0, scale=1 / la)
+    weight_s = (2 * f) / (
+        erf((beta - mu) / (sg * np.sqrt(2)))
+        - erf((alpha - mu) / (sg * np.sqrt(2)))
+    )
+    weight_b = (1 - f) / (np.exp(-la * alpha) - np.exp(-la * beta))
     return (weight_s * pdf_s) + (weight_b * pdf_b)
 
 
@@ -17,17 +20,41 @@ def check_normalisation(pdf, lower, upper):
     integral_value, abserr = quad(pdf, lower, upper)
     return integral_value
 
-def plot_signal_background_mixed(f, la, mu, sg, alpha, beta, filename, npoints=1001):
-    fig = plt.figure(figsize=(10, 8))
-    ax = plt.axes()
+
+def cdf_norm_expon_mixed(x, f, la, mu, sg, alpha, beta):
+    cdf_s = norm.cdf(x, loc=mu, scale=sg) - norm.cdf(
+        alpha * np.ones(x.shape), loc=mu, scale=sg
+    )
+    cdf_b = expon.cdf(x, loc=0, scale=1 / la) - expon.cdf(
+        alpha * np.ones(x.shape), loc=0, scale=1 / la
+    )
+    weight_s = (2 * f) / (
+        erf((beta - mu) / (sg * np.sqrt(2)))
+        - erf((alpha - mu) / (sg * np.sqrt(2)))
+    )
+    weight_b = (1 - f) / (np.exp(-la * alpha) - np.exp(-la * beta))
+    return (weight_s * cdf_s) + (weight_b * cdf_b)
+
+
+def plot_signal_background_mixed(
+    f, la, mu, sg, alpha, beta, filename, npoints=1001, sample_hist=None
+):
+    fig, ax = plt.subplots(figsize=(10, 8))
     x_plot = np.linspace(alpha, beta, npoints)
+    if sample_hist is not None:
+        nh, xe = sample_hist[0], sample_hist[1]
+        cx = 0.5 * (xe[1:] + xe[:-1])
+        ax.errorbar(cx, nh, nh**0.5, fmt="ko")
+        scale_factor = np.sum(nh) * (xe[1] - xe[0])
+    else:
+        scale_factor = 1
     pdf_s = norm.pdf(x_plot, loc=mu, scale=sg)
-    pdf_b = expon.pdf(x_plot, loc=0, scale=1/la)
+    pdf_b = expon.pdf(x_plot, loc=0, scale=1 / la)
     pdf_sb = pdf_norm_expon_mixed(x_plot, f, la, mu, sg, alpha, beta)
-    ax.plot(x_plot, pdf_s)
-    ax.plot(x_plot, pdf_b)
-    ax.plot(x_plot, pdf_sb)
-    plt.savefig("plots/"+filename)
+    ax.plot(x_plot, f * scale_factor * pdf_s)
+    ax.plot(x_plot, (1 - f) * scale_factor * pdf_b)
+    ax.plot(x_plot, scale_factor * pdf_sb)
+    plt.savefig("outputs/" + filename)
 
 
 def rej_sample(pdf, sample_size, lower, upper, max_jobs):
--- src/solve_part_e.py
+++ src/solve_part_e.py
@@ -1,4 +1,36 @@
-from mixed_pdf_tools import pdf_norm_expon_mixed, rej_sample
+import numpy as np
+from iminuit import Minuit
+from iminuit.cost import BinnedNLL
 
-pdf = lambda x: pdf_norm_expon_mixed(x, 0.1, 0.5, 5.28, 0.018, 5.0, 5.6)
-print(rej_sample(pdf, 10, 5.0, 5.6, 100)[0])
+from mixed_pdf_tools import (
+    pdf_norm_expon_mixed,
+    rej_sample,
+    cdf_norm_expon_mixed,
+    plot_signal_background_mixed,
+)
+
+
+# Produce the sample.
+def pdf(x):
+    pdf_norm_expon_mixed(x, 0.1, 0.5, 5.28, 0.018, 5.0, 5.6)
+
+
+sample_array = rej_sample(pdf, 100000, 5.0, 5.6, 100000000)[0]
+
+# Perform Binned-ML fit of parameters.
+nh, xe = np.histogram(sample_array, bins=100, range=(5, 5.6))
+nll = BinnedNLL(nh, xe, cdf_norm_expon_mixed)
+
+cx = 0.5 * (xe[1:] + xe[:-1])
+
+mi = Minuit(nll, f=0.5, la=0.5, mu=5.3, sg=0.1, alpha=5.0, beta=5.6)
+mi.fixed["alpha"] = True
+mi.fixed["beta"] = True
+mi.limits["f"] = (0, 1)
+mi.limits["mu"] = (5.0, 5.6)
+mi.limits["sg"] = (1e-9, 10)
+
+print(mi.migrad())
+plot_signal_background_mixed(
+    *mi.values, "part_e_plot.png", sample_hist=(nh, xe)
+)
