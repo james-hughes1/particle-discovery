c09802c75dac67d4d68a4e50188386efbf2179df
James Hughes <jh2284@cam.ac.uk>
2023-12-14 09:55:57 +0000

    Feat: Improve part d,e plots

--- src/solve_part_e.py
+++ src/solve_part_e.py
@@ -10,7 +10,7 @@ from scipy.stats import chi2
 from mixed_pdf_tools import (
     sample_sb_fast,
     pdf_norm_expon_mixed,
-    cdf_norm_expon_mixed
+    cdf_norm_expon_mixed,
 )
 
 # Sample data from true distribution and separate into bins.
@@ -48,8 +48,7 @@ def plot_sample_fit(
 ):
     plt.figure(figsize=(12, 10))
     grid = gs.GridSpec(
-        2, 2, height_ratios=[3, 1], hspace=0,
-        width_ratios=[6, 1], wspace=0
+        2, 2, height_ratios=[3, 1], hspace=0, width_ratios=[6, 1], wspace=0
     )
     ax1 = plt.subplot(grid[0, 0])
     ax2 = plt.subplot(grid[1, 0])
@@ -72,49 +71,54 @@ def plot_sample_fit(
     weight_b = (1 - f) / (np.exp(-la * alpha) - np.exp(-la * beta))
 
     ax1.plot(
-        x_plot, weight_s * scale_factor * pdf_s, label="Signal density fit"
+        x_plot,
+        weight_s * scale_factor * pdf_s,
+        label="Signal density " "component fit",
     )
     ax1.plot(
-        x_plot, weight_b * scale_factor * pdf_b, label="Background density fit"
+        x_plot,
+        weight_b * scale_factor * pdf_b,
+        label="Background density " "component fit",
     )
-    ax1.plot(
-        x_plot, scale_factor * pdf_sb, label="Total density fit"
-    )
+    ax1.plot(x_plot, scale_factor * pdf_sb, label="Total density fit")
     ax1.set(
-        ylabel="Events", title="Plot Showing Sample with Fitted Densities and"
-        " Pull Plot"
+        ylabel="Events",
+        title="Plot Showing Sample with Fitted Densities and" " Pull Plot",
     )
     ax1.legend()
     residuals = nh - (scale_factor * pdf_norm_expon_mixed(cx, *mi.values))
     pulls = residuals / errors
     ax2.errorbar(cx, pulls, 1, c="k", marker="o", linestyle="")
     ax2.hlines(0, 5.0, 5.6, colors="green")
-    ax2.set(
-        xlabel="M",
-        ylabel="Pull"
-    )
+    ax2.set(xlabel="M", ylabel="Pull")
     pull_range = (np.min(pulls) - 1, np.max(pulls) + 1)
     ax3.hist(
-        pulls, bins=10, range=pull_range, density=True, alpha=0.5,
-        orientation="horizontal"
+        pulls,
+        bins=10,
+        range=pull_range,
+        density=True,
+        alpha=0.5,
+        orientation="horizontal",
     )
     x_pull_plot = np.linspace(*pull_range, 100)
     ax3.plot(
-        norm.pdf(x_pull_plot, loc=0, scale=1), x_pull_plot, c="green",
-        alpha=0.5
+        norm.pdf(x_pull_plot, loc=0, scale=1),
+        x_pull_plot,
+        c="green",
+        alpha=0.5,
     )
     ax3.set_yticks([])
     chi2_score = np.sum(pulls**2)
     dof = len(xe) - 5
     pval = 1 - chi2.cdf(chi2_score, dof)
     ax1.text(
-        x=5.02, y=1800, s="Chi sq./d.o.f = {:.3f}".format(chi2_score/dof),
-        fontsize=18
+        x=5.02,
+        y=1800,
+        s="Chi sq./d.o.f = {:.3f}".format(chi2_score / dof),
+        fontsize=18,
     )
     ax1.text(x=5.02, y=1600, s="P-value = {:.3f}".format(pval), fontsize=18)
     plt.savefig("outputs/" + filename)
 
 
-plot_sample_fit(
-    *mi.values, "part_e_plot.png", nh, xe
-)
+plot_sample_fit(*mi.values, "part_e_plot.png", nh, xe)
